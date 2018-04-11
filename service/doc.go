c64d4869484c55797eb81ab5500395a162dc8b2b
João Ribeiro <joaosoft@gmail.com>
2018-04-11 16:40:57 +0100

    doc

---
+++ service/doc.go
@@ -0,0 +1,8 @@
+/*
+GoWriter helps to create specific writers, eventually to use in a logging service
+like https://github.com/joaosoft/go-log
+
+example at https://github.com/joaosoft/go-writer/tree/master/bin/launcher
+
+*/
+package gowriter
